ce6e609bf6b86e156b742aebcc1dc9a4a56853cf
thhan <thhan@cndfactory.com>
2022-08-16 21:59:56 +0900

    Initial commit

---
+++ src/main/java/com/example/studymarketkurlyadmin/StudyMarketKurlyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.studymarketkurlyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMarketKurlyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMarketKurlyAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studymarketkurlyadmin/StudyMarketKurlyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymarketkurlyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMarketKurlyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
